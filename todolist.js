275f2e58c29f00714ad2c41959d6c7f5dbe3b9f6
SommeanDev <shivanshmourya03@gmail.com>
2023-05-24 21:51:14 +0530

    todolist

---
+++ todolist.js
@@ -0,0 +1,23 @@
+let addTask = document.getElementsByTagName('button');
+let inputText = document.getElementById('inputText');
+let tasks = document.getElementById('tasks');
+
+function add() {
+    var task = document.createElement('li');
+    task.innerHTML = inputText.value;
+    tasks.appendChild(task);
+    inputText.value = '';
+    let serial_task = JSON.stringify(tasks);
+    console.log(serial_task);
+    localStorage.setItem('tasks', serial_task);
+}
+
+function show(){
+    console.log(localStorage.getItem('tasks'));
+}
+
+/*window.onload = function(){
+    var task2 = JSON.parse(localStorage.getItem('tasks'));
+    console.log(task2);
+    tasks.innerHTML = task2;
+};*/
